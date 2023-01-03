47cef331448edaee10ff172fd64812d667881c1f
LindsayH <lindsaynhenderson@gmail.com>
2023-01-03 12:17:20 -0600

    problem with i iteration

--- problem-solving.js
+++ problem-solving.js
@@ -17,11 +17,11 @@
 //4.explore examples with invalid inputs
 //todo write a function which takes in a string and returns counts of each character in the string
 
-charCount("aaaa"); //{a:4}
-charCount("hello"); //{h:1, e:1, l:2, o:1}
+// charCount("aaaa"); //{a:4}
+// charCount("hello"); //{h:1, e:1, l:2, o:1}
 
-//? do we include characters that are not there?
-charCount("aaaa"); //{a:4, b:0. c:0}
+// //? do we include characters that are not there?
+// charCount("aaaa"); //{a:4, b:0. c:0}
 
 //* Explore more complex examples */
 "my phone number is 1234567892"
@@ -29,12 +29,12 @@ charCount("aaaa"); //{a:4, b:0. c:0}
 
 //* Explore examples with empty inputs */
 
-charCount(""); //{Null?; 0?; error?}
+//charCount(""); //{Null?; 0?; error?}
 
 //* Break it down ..Idea is to COMMUNICATE WHAT YOU'RE THINKING
 //* Explicitly write out what steps you need to take
 
-function charCount("Your PIN number is 1234"){
+//function charCount("Your PIN number is 1234"){
   //do something
   //todo We only care about alphanumeric characters, lowercase letters"
 
@@ -49,8 +49,28 @@ function charCount("Your PIN number is 1234"){
   
 
   //return object at end
+//}
+
+//* Solve &&|| simplify problem... if you can't solve a problem, solve a simpler problem.  in simplifying a problem, you can often find answers to the larger problem
+//*a. find the core difficulty
+//*b.temporarily ignore that difficulty
+//*c.write a simplified solution
+//*d.then incorporate that difficulty back in
+
+function charCount(str){
+  //make object to return at end
+  var result = {}
+  //loop over string --for each char
+  for(let i = 0; i < 4; i++){
+    result = i++
+    // //if char is num|| letter, && key in obj, +1 to count, if  num || letter and not in obj, add it to obj and set value to 1
+    // if(result[char] > 0) {
+    //   result[char]++;
+    // } else {
+    //   result[char] = 1
+    // }
+    return result
+  }
 }
-
-
-
-
+  // if char is not letter||num, do nothing
+  //look at each character- is it in the object yet?  if not, add to object and add 1 to count
